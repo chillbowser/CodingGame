08438e67ecec9a913977a5f85185062fe9b3622c
chillbowser <162637015+chillbowser@users.noreply.github.com>
2024-03-07 14:34:47 -0700

    test

--- sketch.js
+++ sketch.js
@@ -5,20 +5,10 @@ let A1
 
 function setup() {
 	new Canvas(1920,1080, 'fullscreen');
-    world.gravity.y = 10
-
-	//Player
-	player = new Sprite(canvas.w/2,canvas.h/2,80,80,'d');
-    player.color = 'blue'
-
-	//Box
-	box = new Sprite(canvas.w/3,canvas.h/2,80,80,'d')
-	box.color = 'orange'
+	background(100)
+	rect(500,500,70,70)
 }
 
 function draw() {
-	background(100);
-	if(kb.pressing('a')){
-		player.x -= 10
-	}
+	
 }
