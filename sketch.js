c2a3e819b25c8e1c05a20106bd157bce3e418ddb
chillbowser <162637015+chillbowser@users.noreply.github.com>
2024-03-07 14:35:44 -0700

    tr

--- sketch.js
+++ sketch.js
@@ -1,14 +1,3 @@
-let player;
-let box;
-let floor;
-let A1
-
-function setup() {
-	new Canvas(1920,1080, 'fullscreen');
+function setup(){
 	background(100)
-	rect(500,500,70,70)
-}
-
-function draw() {
-	
 }
