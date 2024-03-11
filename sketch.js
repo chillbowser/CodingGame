f9735ab141aa0e65b8da6eb8dc1001d6380785fb
chillbowser <162637015+chillbowser@users.noreply.github.com>
2024-03-11 15:23:50 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,121 @@
-function setup(){
-	new Canvas(1920,1080,'fullscreen')
-	background(100)
+let player1,player2;
+let box;
+let floor;
+let nxt = false
+let A1;
+
+function preload(){
+	floor = new Group()
+	floor.collider  = 's'
+	floor.w = 50
+	floor.h = 50
+	floor.tile = '='
+	floor.color = 'gray'
+	floor.friction = 6
+
+	pressure = new Group()
+	pressure.collider = 'n'
+	pressure.w = 50
+	pressure.h = 50
+	pressure.tile = 'p'
+	pressure.color = 'red'
+
+	door = new Group()
+	door.collider = 'n'
+	door.w = 50
+	door.h = 50
+	door.tile = 'd'
+	door.color = 'pink'
+
+	a1 = new Tiles ([
+		'=============================================',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                           =',
+		'=                                          d=',
+		'=                        p                 d=',
+		'=============================================',
+	],floor.w/2,floor.h/2,50,50
+)
+
+}
+function setup() {
+	new Canvas(1920,1080, 'fullscreen');
+    world.gravity.y = 10
+
+	//Players
+	player1 = new Sprite(canvas.w/2,canvas.h/2,80,80,'d');
+    player1.color = 'blue'
+	player1.friction = 2
+	player1.rotationLock = true
+
+	player2 = new Sprite(canvas.w/2 + 100,canvas.h/2,80,80,'d');
+    player2.color = 'purple'
+	player2.friction = 2
+	player2.rotationLock = true
+
+	//Box
+	box = new Sprite(canvas.w/3,canvas.h/2,80,80,'d')
+	box.friction = 1
+	box.rotationLock = true
+	box.color = 'orange'
+}
+
+function draw() {
+	background(100);
+	camera.x = player1.x 
+
+	//controls
+
+	if(kb.pressing('a')){
+		player1.vel.x = -10
+	}
+
+	if(kb.presses('w') && player1.colliding(floor)){
+		player1.vel.y = -10
+	}
+
+	if(kb.pressing('d')){
+		player1.vel.x = 10
+	}
+
+	if(kb.pressing(LEFT_ARROW)){
+		player2.vel.x = -10
+	}
+
+	if(kb.presses(UP_ARROW) && player2.colliding(floor)){
+		player2.vel.y = -10
+	}
+
+	if(kb.pressing(RIGHT_ARROW)){
+		player2.vel.x = 10
+	}
+
+	//pressure plate
+	if(box.overlapping(pressure)){
+		nxt = true
+	} else {
+		nxt = false
+	}
+
+	//door
+
+	if(player1.overlapping(door) || player2.overlapping(door) && nxt == true){
+		console.log('level up')
+	}
 }
